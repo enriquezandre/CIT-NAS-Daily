01e56ea6edf7329a2e90c9d9c2442f11cd72cfd4
Andre Enriquez <andre.enriquez@cit.edu>
2023-09-28 00:27:19 +0800

    Install Flowbite

--- frontend/cit-nas-daily/src/App.jsx
+++ frontend/cit-nas-daily/src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import { Button } from "flowbite-react";
 
 function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button gradientMonochrome="cyan">Cyan</Button>
     </>
   );
 }
