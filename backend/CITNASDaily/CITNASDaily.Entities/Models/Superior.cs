0215051495037a59bd9fba60f4a164c1248eabb9
Jane Carla Barcoma <janecarla.barcoma@cit.edu>
2023-09-29 10:46:12 +0800

    Add Superior Entities

---
+++ backend/CITNASDaily/CITNASDaily.Entities/Models/Superior.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CITNASDaily.Entities.Models
+{
+    public class Superior
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int OfficeId { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string LastName { get; set; }
+    }
+}
