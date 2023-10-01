849e23e20f5adbfe2963e15f121ab82eab825a86
Andre Enriquez <andre.enriquez@cit.edu>
2023-10-02 02:11:32 +0800

    Add placeholder for NAS image avatar

--- frontend/cit-nas-daily/src/components/NAS/NASHeader.jsx
+++ frontend/cit-nas-daily/src/components/NAS/NASHeader.jsx
@@ -5,9 +5,14 @@ export const NASHeader = () => {
     <div className="flex mt-20 ml-10 items-center justify-between">
       <div className="flex items-center">
         <div className="mr-4">
-          <Avatar rounded />
+          {/* Placeholder only, will change if database is finalized in backend */}
+          <Avatar
+            alt="NAS Image"
+            img="/src/placeholders/profile-img.jpg"
+            rounded
+          />
         </div>
-        <div className="text-base">NAS Name</div>
+        <button className="text-base">NAS Name</button>
       </div>
       <button className="text-base text-red-500 mr-10">Logout</button>
     </div>
