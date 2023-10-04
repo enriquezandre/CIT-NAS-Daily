8ecc5d3a59de4fc30d1849f115ea5d9e980b7b44
Andre Enriquez <andre.enriquez@cit.edu>
2023-10-05 01:07:36 +0800

    Add attendance summary card components

--- frontend/cit-nas-daily/src/pages/NAS/AttendanceSummary.jsx
+++ frontend/cit-nas-daily/src/pages/NAS/AttendanceSummary.jsx
@@ -1,6 +1,7 @@
 "use client";
 import { useState } from "react";
 import { Dropdown } from "../../components/Dropdown.jsx";
+import { DataDisplayBox } from "../../components/DataDisplayBox.jsx";
 
 export const AttendanceSummary = () => {
   const [selectedSY, setSelectedSY] = useState("");
@@ -37,23 +38,42 @@ export const AttendanceSummary = () => {
   };
 
   return (
-    <div className="justify-center w-full h-full items-center border border-solid">
-      <div className="font-bold border border-solid ml-2 mt-2">NAS Name</div>
-      <div className="flex mt-2 ml-2">
-        <div className="w-48">
-          SY: <Dropdown options={sy_options} onSelect={handleSelectSY} />
-          <p className="mt-4">Selected Value: {selectedSY}</p>
+    <div className="justify-center w-full h-full items-center border border-solid rounded-lg">
+      <div className="text-primary text-xl font-bold ml-2 my-4">
+        Scholar&rsquo;s Name: NAS Name Here
+      </div>
+      <div className="m-2">
+        <div className="flex mt-2 ml-2">
+          <div className="w-48">
+            SY: <Dropdown options={sy_options} onSelect={handleSelectSY} />
+            <p className="mt-4">Selected Value: {selectedSY}</p>
+          </div>
+          <div className="w-56">
+            SEMESTER:{" "}
+            <Dropdown options={sem_options} onSelect={handleSelectSem} />
+            <p className="mt-4">Selected Value: {selectedSem}</p>
+          </div>
+          <div className="w-48">
+            MONTH:{" "}
+            <Dropdown options={month_options} onSelect={handleSelectedMonth} />
+            <p className="mt-4">Selected Value: {selectedMonth}</p>
+          </div>
         </div>
-        <div className="w-56">
-          SEMESTER:{" "}
-          <Dropdown options={sem_options} onSelect={handleSelectSem} />
-          <p className="mt-4">Selected Value: {selectedSem}</p>
+        <div>
+          <div className="m-2">
+            <div className="flex">
+              <DataDisplayBox label="Number of Lates" data="20" />
+              <DataDisplayBox label="Number of Excused Absences" data="20" />
+              <DataDisplayBox label="Late > 45 Minutes" data="20" />
+            </div>
+            <div className="flex">
+              <DataDisplayBox label="Number of Unexcused Absences" data="20" />
+              <DataDisplayBox label="Late > 10 Minutes" data="20" />
+              <DataDisplayBox label="FTP - Failure to Punch IN/OUT" data="20" />
+            </div>
+          </div>
         </div>
-        <div className="w-48">
-          MONTH:{" "}
-          <Dropdown options={month_options} onSelect={handleSelectedMonth} />
-          <p className="mt-4">Selected Value: {selectedMonth}</p>
-        </div>
+        <div className="m-2">Attendance Summary:</div>
       </div>
     </div>
   );
