79d574b869b4bd9d21e32c159bddc31480d7c2b2
Kaye Belderol <kayecassandra.belderol@cit.edu>
2023-10-06 04:36:20 +0800

    Add route for Superior NAS list

--- frontend/cit-nas-daily/src/App.jsx
+++ frontend/cit-nas-daily/src/App.jsx
@@ -7,6 +7,7 @@ import { Attendance } from "./pages/OAS/Attendance";
 import { Eval } from "./pages/OAS/Eval";
 import { Status } from "./pages/OAS/Status";
 import { Validation } from "./pages/OAS/Validation";
+import { NASList } from "./components/NASList";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
         <Route path="eval" element={<Eval />} />
         <Route path="status" element={<Status />} />
         <Route path="validation" element={<Validation />} />
+        <Route path="naslist" element={<NASList />} />
       </Routes>
     </>
   );
