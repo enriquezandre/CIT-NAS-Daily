454680b205aa71460917a5a11bb51754ad355acf
Stella Mariz Palen <stellamariz.palen@cit.edu>
2023-10-05 15:50:53 +0800

    Add CIT Background in Modal

--- frontend/cit-nas-daily/src/components/OAS/EvaluateGrades.jsx
+++ frontend/cit-nas-daily/src/components/OAS/EvaluateGrades.jsx
@@ -17,12 +17,12 @@ export const EvaluateGrades = ({ show, close }) => {
             <div class="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
                 <div class="relative w-4/6">
                     <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                        <div class="bg-opacity-50">
+                        <div class="bg-[url('/src/assets/CIT.png')] p-12 bg-center bg-no-repeat">
                             <div class="flex flex-col items-center justify-center px-20 py-10 rounded-t">
                                 <p class="text-4xl text-center w-full font-bold text-primary">
                                     GRADE EVALUATION
                                 </p>
-                                <button type="button" class="text-white bg-primary hover:bg-secondary hover:text-primary font-medium rounded-lg text-sm px-5 py-2.5 my-10" onClick={openShowGrades}>EVALUATE GRADES</button>
+                                <button type="button" class="text-white bg-primary hover:bg-secondary hover:text-primary font-medium rounded-lg text-sm px-5 py-2.5 my-10" onClick={openShowGrades}>SHOW GRADES</button>
                                 <ShowGrades show={isViewingShowGrades} close={closeShowGrades} />
                                 <div class="flex flex-row w-full items-center gap-6 mb-10">
                                     <p class="text-xl text-left w-2/4">ALL COURSES PASSED:</p>
@@ -39,7 +39,7 @@ export const EvaluateGrades = ({ show, close }) => {
                                     <p class="text-xl text-left w-3/4">NUMBER OF COURSES FAILED:</p>
                                     <p class="text-xl text-left w-1/4">_____</p>
                                 </div>
-                                <button type="button" class="text-white bg-primary hover:bg-secondary hover:text-primary font-medium rounded-xl text-sm px-20 py-2.5">SUBMIT</button>
+                                <button type="button" class="text-white bg-primary hover:bg-secondary hover:text-primary font-medium rounded-2xl text-sm px-20 py-2.5">SUBMIT</button>
                                 <button type="button" class="text-primary hover:underline font-medium text-sm px-20 py-2.5" onClick={close}>Go Back</button>
                             </div>
                         </div>
