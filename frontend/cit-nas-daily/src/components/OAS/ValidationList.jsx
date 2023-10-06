616c827562839d3e5ae40b7fbfa17cea6f6dd061
Kaye Belderol <kayecassandra.belderol@cit.edu>
2023-10-07 03:01:04 +0800

    Modify ValidationList

--- frontend/cit-nas-daily/src/components/OAS/ValidationList.jsx
+++ frontend/cit-nas-daily/src/components/OAS/ValidationList.jsx
@@ -12,24 +12,34 @@ export const ValidationList = () => {
   return (
     // one place holder
     <div className="grid gap-3">
-      <div className="border border-solid border-2 p-3 flex items-center">
+      <div className="border border-solid border-2 p-3 flex items-center justify-between">
+        <div className="flex items-center">
           <Avatar rounded />
           <span className="ml-5">NAS Name</span>
-        <div className="flex justify-end">
-          <button type="button" class="text-white bg-primary hover:bg-primary hover:text-secondary font-medium rounded-lg text-sm px-5 py-2.5">View Letter</button>
-              {/* Dropdown menu */}
-              <select
-                value={selectedOption}
-                onChange={handleOptionChange}
-                className="ml-2 p-2 border border-solid border-2 rounded-md"
-              >
-                <option value="">Choose Response</option>
-                <option value="excused">Excused</option>
-                <option value="unexcused">Unexcused</option>
-              </select>
-            <button type="button" class="text-white bg-primary hover:bg-primary hover:text-secondary font-medium rounded-lg text-sm px-5 py-2.5">Submit</button>
-          </div>
         </div>
+        <div className="flex items-center space-x-2">
+          <button type="button" class="text-white bg-primary hover:bg-primary hover:text-secondary font-medium rounded-lg text-sm px-5 py-2.5 h-[2.6rem]">View Letter</button>
+          {/* Dropdown menu */}
+          <select
+            value={selectedOption}
+            onChange={handleOptionChange}
+            className="p-2 border border-solid rounded-md h-[2.6rem] bg-[#ebeced]"
+          >
+            <option value="">Choose Response</option>
+            <option value="excused">Excused</option>
+            <option value="unexcused">Unexcused</option>
+            <option value="formakeupduty">For Make Up Duty</option>
+            <option value="approved">Approved</option>
+            <option value="disapproved">Disapproved</option>
+            <option value="warning">Warning</option>
+            <option value="lastwarning">Last Warning</option>
+            <option value="report">Report to Office</option>
+
+          </select>
+          <button type="button" class="text-white bg-primary hover:bg-primary hover:text-secondary font-medium rounded-lg text-sm px-5 py-2.5 h-[2.6rem]">Submit</button>
+        </div>
+      </div>
     </div>
+
   );
 };
