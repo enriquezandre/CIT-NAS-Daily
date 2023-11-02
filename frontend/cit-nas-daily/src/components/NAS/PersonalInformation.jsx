2789734af22689b253ff47bd5b2d99bc1ed07126
Kaye Belderol <kayecassandra.belderol@cit.edu>
2023-11-02 18:24:01 +0800

    Add PersonalInformationComponent

---
+++ frontend/cit-nas-daily/src/components/NAS/PersonalInformation.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export const PersonalInformation = ({ label, id, value, onChange }) => {
+  return (
+    <div>
+      <label htmlFor={id} className="block mb-2 font-bold text-gray-600">
+        {label}
+      </label>
+      <input
+        type="text"
+        id={id}
+        name={id}
+        value={value}
+        onChange={onChange}
+        className="border border-gray-300 rounded-md px-3 py-2 w-full"
+        readOnly={true}
+        style={{ backgroundColor: '#E3E3E3' }}
+      />
+    </div>
+  )
+}
