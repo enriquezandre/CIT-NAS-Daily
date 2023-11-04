79f98b628834badb0b17042e4ed0ea641ef4d887
Kaye Belderol <kayecassandra.belderol@cit.edu>
2023-11-05 00:56:08 +0800

    Modify schedule form

--- frontend/cit-nas-daily/src/pages/NAS/NASSchedule.jsx
+++ frontend/cit-nas-daily/src/pages/NAS/NASSchedule.jsx
@@ -4,25 +4,18 @@ import { Dropdown } from "../../components/Dropdown.jsx";
 export const NASSchedule = () => {
   const [selectedSY, setSelectedSY] = useState("");
   const [selectedSem, setSelectedSem] = useState("");
+  const [schedule, setSchedule] = useState({
+    Monday: { start: "", end: "" },
+    Tuesday: { start: "", end: "" },
+    Wednesday: { start: "", end: "" },
+    Thursday: { start: "", end: "" },
+    Friday: { start: "", end: "" },
+    Saturday: { start: "", end: "" },
+  });
+
   const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
   const sy_options = ["2324", "2223", "2122", "2021"];
   const sem_options = ["First", "Second", "Summer"];
-
-  const initialTimeData = days.map((day) => ({
-    isBroken: false,
-    timeEntries: [
-      {
-        hoursStart: "",
-        minutesStart: "",
-        amPmStart: "AM",
-        hoursEnd: "",
-        minutesEnd: "",
-        amPmEnd: "AM",
-      },
-    ],
-  }));
-
-  const [timeData, setTimeData] = useState(initialTimeData);
 
   const handleSelectSY = (value) => {
     setSelectedSY(value);
@@ -32,85 +25,31 @@ export const NASSchedule = () => {
     setSelectedSem(value);
   };
 
-  const handleHoursStartChange = (value, dayIndex, entryIndex) => {
-    const updatedTimeData = [...timeData];
-    updatedTimeData[dayIndex].timeEntries[entryIndex].hoursStart = value;
-    setTimeData(updatedTimeData);
+  const handleStartTimeChange = (day, value) => {
+    setSchedule({ ...schedule, [day]: { ...schedule[day], start: value } });
   };
 
-  const handleMinutesStartChange = (value, dayIndex, entryIndex) => {
-    const updatedTimeData = [...timeData];
-    updatedTimeData[dayIndex].timeEntries[entryIndex].minutesStart = value;
-    setTimeData(updatedTimeData);
+  const handleEndTimeChange = (day, value) => {
+    setSchedule({ ...schedule, [day]: { ...schedule[day], end: value } });
   };
 
-  const handleAmPmStartChange = (value, dayIndex, entryIndex) => {
-    const updatedTimeData = [...timeData];
-    updatedTimeData[dayIndex].timeEntries[entryIndex].amPmStart = value;
-    setTimeData(updatedTimeData);
-  };
-
-  const handleHoursEndChange = (value, dayIndex, entryIndex) => {
-    const updatedTimeData = [...timeData];
-    updatedTimeData[dayIndex].timeEntries[entryIndex].hoursEnd = value;
-    setTimeData(updatedTimeData);
-  };
-
-  const handleMinutesEndChange = (value, dayIndex, entryIndex) => {
-    const updatedTimeData = [...timeData];
-    updatedTimeData[dayIndex].timeEntries[entryIndex].minutesEnd = value;
-    setTimeData(updatedTimeData);
-  };
-
-  const handleAmPmEndChange = (value, dayIndex, entryIndex) => {
-    const updatedTimeData = [...timeData];
-    updatedTimeData[dayIndex].timeEntries[entryIndex].amPmEnd = value;
-    setTimeData(updatedTimeData);
-  };
-
-  const handleBrokenScheduleChange = (isChecked, dayIndex) => {
-    const updatedTimeData = [...timeData];
-    updatedTimeData[dayIndex].isBroken = isChecked;
-    // If the day is no longer broken, clear any existing time entries
-    if (!isChecked) {
-      updatedTimeData[dayIndex].timeEntries = [];
-    } else if (updatedTimeData[dayIndex].timeEntries.length === 0) {
-      // Add an initial time entry if it's a newly broken day
-      updatedTimeData[dayIndex].timeEntries.push({
-        hoursStart: "",
-        minutesStart: "",
-        amPmStart: "AM",
-        hoursEnd: "",
-        minutesEnd: "",
-        amPmEnd: "AM",
-      });
+  // Function to calculate duty hours for a day
+  const calculateDutyHours = (day) => {
+    const startTime = schedule[day].start;
+    const endTime = schedule[day].end;
+    
+    if (startTime && endTime) {
+      const startHour = parseInt(startTime.split(":")[0]);
+      const startMinute = parseInt(startTime.split(":")[1]);
+      const endHour = parseInt(endTime.split(":")[0]);
+      const endMinute = parseInt(endTime.split(":")[1]);
+      
+      const totalMinutes = (endHour * 60 + endMinute) - (startHour * 60 + startMinute);
+      const totalHours = totalMinutes / 60;
+      return totalHours;
     }
-    setTimeData(updatedTimeData);
-  };
-
-  const addTimeEntry = (dayIndex) => {
-    const updatedTimeData = [...timeData];
-    updatedTimeData[dayIndex].timeEntries.push({
-      hoursStart: "",
-      minutesStart: "",
-      amPmStart: "AM",
-      hoursEnd: "",
-      minutesEnd: "",
-      amPmEnd: "AM",
-    });
-    setTimeData(updatedTimeData);
-  };
-
-  const calculateHours = (startHH, startMM, startAP, endHH, endMM, endAP) => {
-    if (startHH && startMM && startAP && endHH && endMM && endAP) {
-      const startTime = `${startHH}:${startMM} ${startAP}`;
-      const endTime = `${endHH}:${endMM} ${endAP}`;
-      const startDate = new Date(`1970-01-01 ${startTime}`);
-      const endDate = new Date(`1970-01-01 ${endTime}`);
-      const hours = (endDate - startDate) / (1000 * 60 * 60);
-      return hours;
-    }
-    return "Invalid Time";
+    
+    return 0;
   };
 
   return (
@@ -128,117 +67,44 @@ export const NASSchedule = () => {
               <p className="mt-4">Selected Value: {selectedSem}</p>
             </div>
           </div>
-
-          <div className="pt-10">
+          {/* Start of schedule table */}
+          <div className="pt-10"> 
             <table className="w-full">
               <thead>
                 <tr>
                   <th>Day</th>
-                  <th>Broken Sched</th>
+                  <th>Broken Schedule?</th>
                   <th>Start Time</th>
                   <th>End Time</th>
-                  <th>Hours</th>
+                  <th>Total Hours</th>
                 </tr>
               </thead>
               <tbody>
-                {days.map((day, dayIndex) => (
+                {days.map((day) => (
                   <tr key={day}>
-                    <td className="text-center p-5">{day}</td>
-                    <td className="text-center p-5">
+                    <td className="text-center pt-5">{day}</td>
+                    <td className="text-center">yes</td>
+                    <td className="text-center">
                       <input
-                        type="checkbox"
-                        onChange={(e) => handleBrokenScheduleChange(e.target.checked, dayIndex)}
-                        checked={timeData[dayIndex].isBroken}
+                        type="time"
+                        value={schedule[day].start}
+                        onChange={(e) => handleStartTimeChange(day, e.target.value)}
                       />
                     </td>
-                    <td className="text-center p-5">
-                      {timeData[dayIndex].isBroken &&
-                        timeData[dayIndex].timeEntries.map((timeEntry, entryIndex) => (
-                          <div key={entryIndex}>
-                            <input
-                              type="text"
-                              placeholder="HH"
-                              value={timeEntry.hoursStart}
-                              onChange={(e) => handleHoursStartChange(e.target.value, dayIndex, entryIndex)}
-                              className="w-10"
-                            />
-                            :
-                            <input
-                              type="text"
-                              placeholder="MM"
-                              value={timeEntry.minutesStart}
-                              onChange={(e) => handleMinutesStartChange(e.target.value, dayIndex, entryIndex)}
-                              className="w-10"
-                            />
-                            <select
-                              value={timeEntry.amPmStart}
-                              onChange={(e) => handleAmPmStartChange(e.target.value, dayIndex, entryIndex)}
-                              className="w-16"
-                            >
-                              <option value="AM">AM</option>
-                              <option value="PM">PM</option>
-                            </select>
-                            <br />
-                          </div>
-                        ))}
+                    <td className="text-center">
+                      <input
+                        type="time"
+                        value={schedule[day].end}
+                        onChange={(e) => handleEndTimeChange(day, e.target.value)}
+                      />
                     </td>
-                    <td className="text-center p-5">
-                      {timeData[dayIndex].isBroken &&
-                        timeData[dayIndex].timeEntries.map((timeEntry, entryIndex) => (
-                          <div key={entryIndex}>
-                            <input
-                              type="text"
-                              placeholder="HH"
-                              value={timeEntry.hoursEnd}
-                              onChange={(e) => handleHoursEndChange(e.target.value, dayIndex, entryIndex)}
-                              className="w-10"
-                            />
-                            :
-                            <input
-                              type="text"
-                              placeholder="MM"
-                              value={timeEntry.minutesEnd}
-                              onChange={(e) => handleMinutesEndChange(e.target.value, dayIndex, entryIndex)}
-                              className="w-10"
-                            />
-                            <select
-                              value={timeEntry.amPmEnd}
-                              onChange={(e) => handleAmPmEndChange(e.target.value, dayIndex, entryIndex)}
-                              className="w-16"
-                            >
-                              <option value="AM">AM</option>
-                              <option value="PM">PM</option>
-                            </select>
-                            <br />
-                          </div>
-                        ))}
-                    </td>
-                    <td className="text-center p-5">
-                      {timeData[dayIndex].isBroken &&
-                        timeData[dayIndex].timeEntries.map((timeEntry, entryIndex) => (
-                          <div key={entryIndex}>
-                            {calculateHours(
-                              timeEntry.hoursStart,
-                              timeEntry.minutesStart,
-                              timeEntry.amPmStart,
-                              timeEntry.hoursEnd,
-                              timeEntry.minutesEnd,
-                              timeEntry.amPmEnd
-                            )}
-                            <br />
-                          </div>
-                        ))}
-                    </td>
-                    <td className="text-center p-5">
-                      {timeData[dayIndex].isBroken && (
-                        <button onClick={() => addTimeEntry(dayIndex)}>Add Time</button>
-                      )}
-                    </td>
+                    <td className="text-center">{calculateDutyHours(day)} hours</td>
                   </tr>
                 ))}
               </tbody>
             </table>
           </div>
+          {/* End of schedule table */}
         </div>
       </div>
     </div>
