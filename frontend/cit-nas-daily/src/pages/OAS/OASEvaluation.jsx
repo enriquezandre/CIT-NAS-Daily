cbf5f610b963aea772442662f32b31fc6052ca9f
Andre Enriquez <andre.enriquez@cit.edu>
2023-11-07 07:51:45 +0800

    Add partial implementation in evaluation page

--- frontend/cit-nas-daily/src/pages/OAS/OASEvaluation.jsx
+++ frontend/cit-nas-daily/src/pages/OAS/OASEvaluation.jsx
@@ -1,7 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { Button } from "flowbite-react";
+import { HiOutlineArrowLeft, HiOutlineArrowRight } from "react-icons/hi";
+import axios from "axios";
 
 export const OASEvaluation = () => {
+  const [selectedSY, setSelectedSY] = useState("2324");
+  const [selectedSem, setSelectedSem] = useState("First");
   const [selectedOptions, setSelectedOptions] = useState({});
+  const [firstName, setFirstname] = useState("");
+  const [lastName, setLastname] = useState("");
+  const [middleName, setMiddlename] = useState("");
+  const [office, setOffice] = useState("");
+  const sy_options = ["2324", "2223", "2122", "2021"];
+  const sem_options = ["First", "Second", "Summer"];
+  const [nasId, setNasId] = useState(1);
 
   const handleOptionChange = (rowName, selectedValue) => {
     setSelectedOptions({
@@ -46,20 +58,84 @@ export const OASEvaluation = () => {
     },
   ];
 
+  useEffect(() => {
+    const fetchNas = async () => {
+      try {
+        // Create an Axios instance with the Authorization header
+        const api = axios.create({
+          baseURL: "https://localhost:7001/api",
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        });
+
+        const nasresponse = await api.get(`/NAS/${nasId}`);
+        console.log(nasresponse);
+        const nasData = nasresponse.data;
+
+        const officeResponse = await api.get(`Offices/${nasId}/NAS`);
+        const officeData = officeResponse.data;
+
+        setFirstname(nasData.firstName);
+        setMiddlename(nasData.middleName);
+        setLastname(nasData.lastName);
+        setOffice(officeData.name);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchNas();
+
+    console.log("Selected Sem:", selectedSem);
+    console.log("Selected SY:", selectedSY);
+  }, [selectedSY, selectedSem, nasId]);
+
+  const handleSelectSY = (event) => {
+    const value = event.target.value;
+    setSelectedSY(value);
+  };
+
+  const handleSelectSem = (event) => {
+    const value = event.target.value;
+    setSelectedSem(value);
+  };
+
   return (
     <>
       <div className="flex rounded-lg border border-gray-200 bg-white shadow-md dark:border-gray-700 dark:bg-gray-800 flex-col w-9/10 mx-8 mb-10">
         <div className="flex h-full flex-col justify-center">
-          <ul className="flex-wrap items-center text-lg font-medium rounded-t-lg bg-grey px-8 py-4 grid grid-cols-3">
+          <ul className="flex-wrap items-center text-lg font-medium rounded-t-lg bg-grey pr-4 py-4 grid grid-cols-3">
+            <div
+              className={`flex items-center w-auto ${
+                nasId === 1 ? "ml-10" : ""
+              }`}
+            >
+              <div>
+                {nasId > 1 && (
+                  <Button
+                    className="text-black"
+                    onClick={() => setNasId(nasId - 1)}
+                  >
+                    <HiOutlineArrowLeft className="h-6 w-6" />
+                  </Button>
+                )}
+              </div>
+              <div className="font-bold" style={{ textTransform: "uppercase" }}>
+                NAS NAME: {lastName}, {firstName} {middleName}
+              </div>
+            </div>
             <li>
-              <p className="font-bold">BELDEROL, KAYE CASSANDRA</p>
+              <p
+                className="font-bold text-center"
+                style={{ textTransform: "uppercase" }}
+              >
+                DEPT/OFFICE: {office}
+              </p>
             </li>
-            <li>
-              <p className="font-bold text-center">DEPT./OFFICE: ETO</p>
-            </li>
-            <li>
-              <div className="flex justify-end">
-                <div className="relative w-4/5">
+            <li className="flex justify-end">
+              <div className="flex ">
+                <div className="relative w-auto">
                   <input
                     type="search"
                     className="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded border"
@@ -88,28 +164,47 @@ export const OASEvaluation = () => {
                   </button>
                 </div>
               </div>
+              <Button
+                className="text-black"
+                onClick={() => setNasId(nasId + 1)}
+              >
+                <HiOutlineArrowRight className="h-6 w-6" />
+              </Button>
             </li>
           </ul>
           <div className="px-8 py-4">
             <div className="flex flex-row justify-start items-center gap-10 mt-2 mb-8">
               <div className="flex flex-row gap-2 items-center">
-                <p>SY:</p>
+                <div className="mr-2">SY:</div>
                 <select
-                  id="month"
-                  name="month"
-                  className="block w-full pl-3 pr-10 py-2 text-base border rounded-md"
+                  id="sy"
+                  name="sy"
+                  value={selectedSY}
+                  onChange={handleSelectSY}
+                  className=" w-full text-base border rounded-md"
                 >
-                  <option value="">2324</option>
+                  {Array.isArray(sy_options) &&
+                    sy_options.map((sy, index) => (
+                      <option key={index} value={sy}>
+                        {sy}
+                      </option>
+                    ))}
                 </select>
               </div>
               <div className="flex flex-row gap-2 items-center">
-                <p>SEMESTER:</p>
+                <div className="mr-2">SEMESTER:</div>
                 <select
-                  id="month"
-                  name="month"
-                  className="block w-full pl-3 pr-10 py-2 text-base border rounded-md"
+                  id="sem"
+                  name="sem"
+                  value={selectedSem}
+                  onChange={handleSelectSem}
+                  className=" w-full text-base border rounded-md"
                 >
-                  <option value="">SECOND</option>
+                  {sem_options.map((sem, index) => (
+                    <option key={index} value={sem}>
+                      {sem}
+                    </option>
+                  ))}
                 </select>
               </div>
             </div>
