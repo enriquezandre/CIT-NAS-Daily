f7462b41cc257a47f197eb7ceb4b13f86b81c5e1
Andre Enriquez <andre.enriquez@cit.edu>
2023-11-07 07:39:19 +0800

    Implement attendance summary in OAS side

--- frontend/cit-nas-daily/src/components/OAS/WeeklyAttendance.jsx
+++ frontend/cit-nas-daily/src/components/OAS/WeeklyAttendance.jsx
@@ -1,8 +1,15 @@
 import { useEffect, useState } from "react";
+import PropTypes from "prop-types";
 import axios from "axios";
 
-export const WeeklyAttendance = () => {
+export const WeeklyAttendance = ({
+  nasId,
+  selectedMonth,
+  selectedSem,
+  selectedSY,
+}) => {
   const [attendanceSummaries, setAttendanceSummaries] = useState([]);
+
   useEffect(() => {
     const fetchNas = async () => {
       try {
@@ -13,9 +20,19 @@ export const WeeklyAttendance = () => {
           },
         });
 
-        const response = await api.get(`BiometricLogs?nasId=1`);
-        console.log(response);
+        const response = await api.get(`BiometricLogs?nasId=${nasId}`);
         const data = response.data;
+
+        // TO DO: DATE RANGE FOR FIRST, SECOND, SUMMER SEMESTER
+        const startDate = new Date("2023-10-01");
+        const endDate = new Date();
+        endDate.setHours(23, 59, 59, 999);
+        const dateRange = [];
+        for (let d = startDate; d <= endDate; d.setDate(d.getDate() + 1)) {
+          dateRange.push(new Date(d));
+        }
+
+        // Group the data by date
         const groupedData = data.reduce((acc, curr) => {
           const date = curr.dateTime.split("T")[0];
           if (!acc[date]) {
@@ -23,39 +40,82 @@ export const WeeklyAttendance = () => {
               DutyOn: [],
               DutyOff: [],
               OvertimeOn: [],
-              OverTimeOff: [],
+              OvertimeOff: [],
             };
           }
           acc[date][curr.inOut].push(curr);
           return acc;
         }, {});
 
-        const latestLogs = Object.keys(groupedData).map((date) => {
-          const logs = groupedData[date];
-          return {
-            dateTime: date,
-            DutyOn: logs.DutyOn.sort(
-              (a, b) => new Date(b.dateTime) - new Date(a.dateTime)
-            )[0],
-            DutyOff: logs.DutyOff.sort(
-              (a, b) => new Date(b.dateTime) - new Date(a.dateTime)
-            )[0],
-            OvertimeOn: logs.OvertimeOn.sort(
-              (a, b) => new Date(b.dateTime) - new Date(a.dateTime)
-            )[0],
-            OverTimeOff: logs.OverTimeOff.sort(
-              (a, b) => new Date(b.dateTime) - new Date(a.dateTime)
-            )[0],
-          };
+        // Create a record for each date in the date range
+        const latestLogs = dateRange.map((date) => {
+          const dateString = date.toISOString().split("T")[0];
+          const logs = groupedData[dateString];
+          if (logs) {
+            return {
+              dateTime: dateString,
+              DutyOn: logs.DutyOn.sort(
+                (a, b) => new Date(b.dateTime) - new Date(a.dateTime)
+              )[0],
+              DutyOff: logs.DutyOff.sort(
+                (a, b) => new Date(b.dateTime) - new Date(a.dateTime)
+              )[0],
+              OvertimeOn: logs.OvertimeOn.sort(
+                (a, b) => new Date(b.dateTime) - new Date(a.dateTime)
+              )[0],
+              OvertimeOff: logs.OvertimeOff.sort(
+                (a, b) => new Date(b.dateTime) - new Date(a.dateTime)
+              )[0],
+            };
+          } else {
+            return {
+              dateTime: dateString,
+              DutyOn: null,
+              DutyOff: "absent",
+              OvertimeOn: null,
+              OverTimeOff: null,
+            };
+          }
         });
-        setAttendanceSummaries(latestLogs);
+
+        const filteredData = latestLogs.filter((item) => {
+          const date = new Date(item.dateTime);
+          const month = date.getMonth();
+          const year = date.getFullYear();
+          const first = parseInt(
+            year.toString().substring(0, 2) + selectedSY.substring(0, 2)
+          );
+          const second = parseInt(
+            year.toString().substring(0, 2) + selectedSY.substring(2)
+          );
+          switch (selectedMonth) {
+            case -1:
+              return month >= 7 && month <= 11 && year === first;
+            case -2:
+              return month >= 0 && month <= 5 && year === second;
+            case -3:
+              return month >= 5 && month <= 7 && year === second;
+          }
+
+          switch (selectedSem) {
+            case "First":
+              return month === selectedMonth && year === first;
+            case "Second":
+              return month === selectedMonth && year === second;
+            case "Summer":
+              return month === selectedMonth && year === second;
+          }
+        });
+        console.log(nasId);
+        setAttendanceSummaries(filteredData);
+        console.log(filteredData);
       } catch (error) {
         console.error(error);
       }
     };
 
     fetchNas();
-  }, [1]);
+  }, [nasId, selectedMonth, selectedSem, selectedSY]);
 
   const formatTime = (time) => {
     const [hour, minute] = time.split(":");
@@ -86,7 +146,9 @@ export const WeeklyAttendance = () => {
             </td>
             <td className="border px-4 py-2 w-1/5 text-center">
               {summary.DutyOff
-                ? formatTime(summary.DutyOff.dateTime.split("T")[1])
+                ? summary.DutyOff === "absent"
+                  ? "absent"
+                  : formatTime(summary.DutyOff.dateTime.split("T")[1])
                 : ""}
             </td>
             <td className="border px-4 py-2 w-1/5 text-center">
@@ -104,4 +166,11 @@ export const WeeklyAttendance = () => {
       </tbody>
     </table>
   );
+};
+
+WeeklyAttendance.propTypes = {
+  nasId: PropTypes.number.isRequired,
+  selectedMonth: PropTypes.number.isRequired,
+  selectedSem: PropTypes.string.isRequired,
+  selectedSY: PropTypes.string.isRequired,
 };
