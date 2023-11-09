e03b0829dd70d9f9571776e21b9f807c966beb4a
Stella Mariz Palen <stellamariz.palen@cit.edu>
2023-11-09 23:42:49 +0800

    Change from string to byte[]

---
+++ backend/CITNASDaily/CITNASDaily.Entities/Dtos/NASDtos/NASPhotoUpdateDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CITNASDaily.Entities.Dtos.NASDtos
+{
+    public class NASPhotoUpdateDto
+    {
+        public int Id { get; set; }
+        public byte[]? Image { get; set; }
+    }
+}
