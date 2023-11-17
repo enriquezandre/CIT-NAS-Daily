f415d49a922da53679f7c8d12efe4c3f91c66223
Kaye Belderol <kayecassandra.belderol@cit.edu>
2023-11-17 22:41:29 +0800

    refractor upload grades modal

--- frontend/cit-nas-daily/src/components/NAS/UploadGradesModal.jsx
+++ frontend/cit-nas-daily/src/components/NAS/UploadGradesModal.jsx
@@ -1,81 +1,98 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import PropTypes from "prop-types";
 import { Modal } from "flowbite-react";
 
-export const UploadGradesModal = ({ isOpen, closeModal }) => {
+export const UploadGradesModal = ({ isOpen, closeModal, handleSubmit }) => {
   const fileInputRef = useRef(null);
+  const [selectedFileName, setSelectedFileName] = useState(null);
 
   const handleFileChange = (event) => {
     const selectedFile = event.target.files[0];
     console.log("Selected file:", selectedFile);
+    setSelectedFileName(selectedFile ? selectedFile.name : null);
   };
 
   const handleButtonClick = () => {
     fileInputRef.current.click();
   };
 
+  const handleConfirm = () => {
+    // Add any additional logic or validation before calling handleSubmit
+    handleSubmit();
+    closeModal();
+  };
+
   return (
     <div>
       {isOpen && (
-        <Modal
-          show={isOpen}
-          className="rounded-2xl"
+        <div
           style={{
-            padding: "0",
-            zIndex: 1000,
-            width: "30rem",
-            left: "50%",
-            transform: "translateX(-50%)",
+            position: "fixed",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            background: "rgba(0, 0, 0, 0.3)",
+            zIndex: 999,
+          }}
+        ></div>
+      )}
+
+      <Modal
+        show={isOpen}
+        className="rounded-2xl"
+        style={{
+          padding: "0",
+          zIndex: 1000,
+          width: "30rem",
+          left: "50%",
+          transform: "translateX(-50%)",
+        }}
+      >
+        <Modal.Body>
+          <div style={{ display: "flex", flexDirection: "column", alignItems: "flex-start" }}>
+            <div>
+              <p className="font-bold text-lg">Upload Excuse Letter</p>
+            </div>
+            <div className="pt-5">
+              <input
+                type="file"
+                onChange={handleFileChange}
+                ref={fileInputRef}
+                style={{ display: "none" }}
+              />
+              <button className="bg-primary text-white py-1 px-5" onClick={handleButtonClick}>
+                Choose Files
+              </button>
+              <span className="ml-3">{selectedFileName || "No file selected"}</span>
+            </div>
+          </div>
+        </Modal.Body>
+        <Modal.Footer
+          style={{
+            paddingTop: "0.3rem",
+            paddingBottom: "0.3rem",
+            display: "flex",
+            justifyContent: "flex-end",
           }}
         >
-          <Modal.Body>
-            <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-              <div className="pb-5"></div>
-              <div className="text-left">
-                <p className="font-bold text-lg">Upload Excuse letter</p>
-              </div>
-              <div className="pt-5">
-                <input
-                  type="file"
-                  onChange={handleFileChange}
-                  ref={fileInputRef}
-                  style={{ display: "none" }}
-                />
-                <button className="bg-primary text-white py-2 px-5" onClick={handleButtonClick}>
-                  Choose Files
-                </button>
-              </div>
+          <div className="flex justify-end items-center">
+            <div className="flex m-2">
+              <button className="bg-primary text-white py-2 px-6 rounded-full" onClick={closeModal}>
+                Cancel
+              </button>
             </div>
-          </Modal.Body>
-          <Modal.Footer
-            style={{
-              paddingTop: "0.3rem",
-              paddingBottom: "0.3rem",
-              display: "flex",
-              justifyContent: "flex-end",
-            }}
-          >
-            <div className="flex justify-end items-center">
-              <div className="flex m-2">
-                <button
-                  className="bg-primary text-white py-2 px-6 rounded-full"
-                  onClick={closeModal}
-                >
-                  Cancel
-                </button>
-              </div>
-              <div className="flex m-2">
-                <button
-                  className="bg-primary text-white py-2 px-6 rounded-full"
-                  // onClick={handleConfirm}
-                >
-                  Submit
-                </button>
-              </div>
+            <div className="flex m-2">
+              <button
+                className="bg-primary text-white py-2 px-6 rounded-full"
+                onClick={handleConfirm}
+              >
+                Submit
+              </button>
             </div>
-          </Modal.Footer>
-        </Modal>
-      )}
+          </div>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 };
@@ -83,4 +100,5 @@ export const UploadGradesModal = ({ isOpen, closeModal }) => {
 UploadGradesModal.propTypes = {
   isOpen: PropTypes.bool,
   closeModal: PropTypes.func,
+  handleSubmit: PropTypes.func,
 };
