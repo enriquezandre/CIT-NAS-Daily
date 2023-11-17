02da6ee2819f485dd1861dfc562c422e6ea8a40a
Andre Enriquez <andre.enriquez@cit.edu>
2023-11-17 16:07:25 +0800

    Add specific NAS Evaluation page

---
+++ frontend/cit-nas-daily/src/components/OAS/SpecificNASEvaluation.jsx
@@ -0,0 +1,174 @@
+import { useState, useEffect } from "react";
+import { SuperiorEval } from "../../components/SuperiorEval";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+
+export const SpecificNASEvaluation = () => {
+  const [selectedSY, setSelectedSY] = useState("2324");
+  const [selectedSem, setSelectedSem] = useState("First");
+  const [firstName, setFirstname] = useState("");
+  const [lastName, setLastname] = useState("");
+  const [middleName, setMiddlename] = useState("");
+  const [office, setOffice] = useState("");
+  const sy_options = ["2324", "2223", "2122", "2021"];
+  const sem_options = ["First", "Second", "Summer"];
+  const nasId = useParams().nasId;
+  const handleSelectSY = (event) => {
+    const value = event.target.value;
+    setSelectedSY(value);
+  };
+
+  const handleSelectSem = (event) => {
+    const value = event.target.value;
+    setSelectedSem(getSemesterValue(value));
+  };
+
+  function getSemesterValue(sem) {
+    switch (sem) {
+      case "First":
+        return 0;
+      case "Second":
+        return 1;
+      case "Summer":
+        return 3;
+      default:
+        return "Invalid semester";
+    }
+  }
+
+  useEffect(() => {
+    const fetchNas = async () => {
+      try {
+        // Create an Axios instance with the Authorization header
+        const api = axios.create({
+          baseURL: "https://localhost:7001/api",
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        });
+
+        const nasResponse = await api.get(`/NAS/${nasId}`);
+        console.log(nasResponse);
+        const nasData = nasResponse.data;
+
+        const officeResponse = await api.get(`Offices/${nasId}/NAS`);
+        const officeData = officeResponse.data;
+
+        setFirstname(nasData.firstName);
+        setMiddlename(nasData.middleName);
+        setLastname(nasData.lastName);
+        setOffice(officeData.name);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchNas();
+
+    console.log("Selected Sem:", selectedSem);
+    console.log("Selected SY:", selectedSY);
+  }, [selectedSY, selectedSem, nasId]);
+
+  return (
+    <>
+      <div className="flex rounded-lg border border-gray-200 bg-white shadow-md dark:border-gray-700 dark:bg-gray-800 flex-col w-9/10 mx-8 mb-10">
+        <div className="flex h-full flex-col justify-center">
+          <ul className="flex-wrap items-center text-lg font-medium rounded-t-lg bg-grey pr-4 py-4 grid grid-cols-3">
+            <div
+              className="font-bold ml-10"
+              style={{ textTransform: "uppercase" }}
+            >
+              NAS NAME: {lastName}, {firstName} {middleName}
+            </div>
+            <li>
+              <p
+                className="font-bold text-center"
+                style={{ textTransform: "uppercase" }}
+              >
+                DEPT/OFFICE: {office}
+              </p>
+            </li>
+            <li className="flex justify-end">
+              <div className="flex ">
+                <div className="relative w-auto">
+                  <input
+                    type="search"
+                    className="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded border"
+                    placeholder="Search NAS..."
+                    required
+                  />
+                  <button
+                    type="submit"
+                    className="absolute top-0 right-0 p-2.5 text-sm font-medium h-full"
+                  >
+                    <svg
+                      className="w-4 h-4"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 20 20"
+                    >
+                      <path
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                      />
+                    </svg>
+                  </button>
+                </div>
+              </div>
+            </li>
+          </ul>
+          <div className="px-8 py-4">
+            <div className="flex flex-row justify-start items-center gap-10 mt-2 mb-8">
+              <div className="flex flex-row gap-2 items-center">
+                <div className="mr-2">SY:</div>
+                <select
+                  id="sy"
+                  name="sy"
+                  value={selectedSY}
+                  onChange={handleSelectSY}
+                  className=" w-full text-base border rounded-md"
+                >
+                  {Array.isArray(sy_options) &&
+                    sy_options.map((sy, index) => (
+                      <option key={index} value={sy}>
+                        {sy}
+                      </option>
+                    ))}
+                </select>
+              </div>
+              <div className="flex flex-row gap-2 items-center">
+                <div className="mr-2">SEMESTER:</div>
+                <select
+                  id="sem"
+                  name="sem"
+                  value={selectedSem}
+                  onChange={handleSelectSem}
+                  className=" w-full text-base border rounded-md"
+                >
+                  {sem_options.map((sem, index) => (
+                    <option key={index} value={sem}>
+                      {sem}
+                    </option>
+                  ))}
+                </select>
+              </div>
+            </div>
+            <hr className="my-5 border-t-2 border-gray-300" />
+            <div>
+              {" "}
+              <SuperiorEval
+                nasId={nasId}
+                selectedSem={getSemesterValue(selectedSem)}
+                selectedSY={selectedSY}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
