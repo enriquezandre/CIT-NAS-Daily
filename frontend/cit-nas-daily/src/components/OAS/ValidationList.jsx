e8ebece024b74163be686c60d35e703b8cc47fd6
Kaye Belderol <kayecassandra.belderol@cit.edu>
2023-11-19 23:56:36 +0800

    Integrate Validation list

--- frontend/cit-nas-daily/src/components/OAS/ValidationList.jsx
+++ frontend/cit-nas-daily/src/components/OAS/ValidationList.jsx
@@ -1,45 +1,96 @@
-import { Card } from "flowbite-react";
 import { Avatar } from "flowbite-react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 export const ValidationList = () => {
-  const [selectedOption, setSelectedOption] = useState("");
+  const [validation, setValidation] = useState([]);
 
-  const handleOptionChange = (event) => {
-    setSelectedOption(event.target.value);
+  const formatDateString = (dateString) => {
+    const options = {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+      hour: "numeric",
+      minute: "numeric",
+      hour12: true,
+    };
+
+    const date = new Date(dateString);
+    return date.toLocaleString("en-US", options);
   };
 
+  const viewPDF = (base64String) => {
+    // Open a new window with the PDF content
+    const newWindow = window.open();
+    newWindow.document.write(
+      `<iframe width='100%' height='100%' src='data:application/pdf;base64,${base64String}'></iframe>`
+    );
+  };
+
+  useEffect(() => {
+    const fetchValidation = async () => {
+      try {
+        const api = axios.create({
+          baseURL: "https://localhost:7001/api",
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        });
+
+        const response = await api.get("/Validation");
+        const validationData = await Promise.all(
+          response.data.map(async (item) => {
+            // Fetch NAS details using nasId
+            const nasResponse = await api.get(`/NAS/${item.nasId}`);
+            // Add NAS name to the item
+            return {
+              ...item,
+              nasName: nasResponse.data.fullName,
+            };
+          })
+        );
+
+        setValidation(validationData);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchValidation();
+  }, []);
+
   return (
-    // one place holder
     <div className="grid gap-3">
-      <div className="border border-solid border-2 p-3 flex items-center justify-between">
-        <div className="flex items-center">
-          <Avatar rounded />
-          <span className="ml-5">NAS Name</span>
+      {validation.map((item) => (
+        <div
+          key={item.id}
+          className="border border-solid rounded-md p-3 flex items-center justify-between"
+        >
+          <div className="flex items-center">
+            <Avatar rounded />
+            <p className="ml-5">
+              <span>{item.nasName}</span>
+              <br />
+              <span className="text-xs">{formatDateString(item.dateSubmitted)}</span>
+            </p>
+          </div>
+          <div className="flex items-center space-x-2">
+            <button
+              type="button"
+              className="text-white bg-primary hover:bg-primary hover:text-secondary font-medium rounded-lg text-sm px-5 py-2.5 h-[2.6rem]"
+              onClick={() => viewPDF(item.nasLetter)}
+            >
+              View Letter
+            </button>
+            <button
+              type="button"
+              className="text-white bg-primary hover:bg-primary hover:text-secondary font-medium rounded-lg text-sm px-5 py-2.5 h-[2.6rem]"
+            >
+              Update status
+            </button>
+          </div>
         </div>
-        <div className="flex items-center space-x-2">
-          <button type="button" class="text-white bg-primary hover:bg-primary hover:text-secondary font-medium rounded-lg text-sm px-5 py-2.5 h-[2.6rem]">View Letter</button>
-          {/* Dropdown menu */}
-          <select
-            value={selectedOption}
-            onChange={handleOptionChange}
-            className="p-2 border border-solid rounded-md h-[2.6rem] bg-[#ebeced]"
-          >
-            <option value="">Choose Response</option>
-            <option value="excused">Excused</option>
-            <option value="unexcused">Unexcused</option>
-            <option value="formakeupduty">For Make Up Duty</option>
-            <option value="approved">Approved</option>
-            <option value="disapproved">Disapproved</option>
-            <option value="warning">Warning</option>
-            <option value="lastwarning">Last Warning</option>
-            <option value="report">Report to Office</option>
-
-          </select>
-          <button type="button" class="text-white bg-primary hover:bg-primary hover:text-secondary font-medium rounded-lg text-sm px-5 py-2.5 h-[2.6rem]">Submit</button>
-        </div>
-      </div>
+      ))}
     </div>
-
   );
 };
