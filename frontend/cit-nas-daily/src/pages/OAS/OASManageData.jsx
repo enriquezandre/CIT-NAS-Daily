c013745aa9af911c36028dc8bfcaa0ac14c50c0f
Kaye Belderol <kayecassandra.belderol@cit.edu>
2023-12-02 01:00:48 +0800

    Remove padding for alignment

--- frontend/cit-nas-daily/src/pages/OAS/OASManageData.jsx
+++ frontend/cit-nas-daily/src/pages/OAS/OASManageData.jsx
@@ -195,7 +195,7 @@ export const OASManageData = () => {
 
   return (
     <>
-      <div className="flex rounded-lg border border-gray-200 bg-white shadow-md dark:border-gray-700 dark:bg-gray-800 flex-col w-9/10 mx-8 mb-10">
+      <div className="flex rounded-lg border border-gray-200 bg-white shadow-md dark:border-gray-700 dark:bg-gray-800 flex-col w-9/10 mb-10">
         <div className="flex h-full flex-col justify-center">
           <ul className="flex-wrap items-center text-lg font-medium rounded-t-lg bg-grey pr-4 py-4 grid grid-cols-2">
             <div className="flex flex-row items-center">
