dd3637afa835911aa91ff5c9db7071dff1cb2e8e
Kaye Belderol <kayecassandra.belderol@cit.edu>
2023-12-05 20:14:19 +0800

    Create reusable dropdown component

--- frontend/cit-nas-daily/src/components/Dropdown.jsx
+++ frontend/cit-nas-daily/src/components/Dropdown.jsx
@@ -1,65 +1,28 @@
-import { useState } from "react";
 import PropTypes from "prop-types";
 
-export const Dropdown = ({ options, onSelect }) => {
-  const [selectedOption, setSelectedOption] = useState(options[0]);
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleDropdown = () => {
-    setIsOpen(!isOpen);
-  };
-
-  const handleOptionClick = (option) => {
-    setSelectedOption(option);
-    onSelect(option);
-    setIsOpen(false);
-  };
-
+export const Dropdown = ({ label, options, selectedValue, onChange }) => {
   return (
-    <div className="relative inline-block text-left">
-      <div>
-        <button
-          type="button"
-          className="border border-solid px-1 rounded inline-flex items-center"
-          onClick={toggleDropdown}
-        >
-          {selectedOption}
-          <svg
-            className="mr-1 ml-2 h-5 w-5"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            aria-hidden="true"
-          >
-            <path
-              fillRule="evenodd"
-              d="M6.293 9.293a1 1 0 011.414 0L10 11.586l2.293-2.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z"
-              clipRule="evenodd"
-            />
-          </svg>
-        </button>
-      </div>
-
-      {isOpen && (
-        <div className="origin-top-right absolute right-0 mt-2 w-28 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
-          <div className="py-1">
-            {options.map((option) => (
-              <button
-                key={option}
-                className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 w-full text-left"
-                onClick={() => handleOptionClick(option)}
-              >
-                {option}
-              </button>
-            ))}
-          </div>
-        </div>
-      )}
+    <div className="flex flex-row gap-2 items-center">
+      <div className="mr-2">{label}:</div>
+      <select
+        value={selectedValue}
+        onChange={onChange}
+        className="w-full text-base border rounded-md"
+        style={{ width: "6rem" }}
+      >
+        {options.map((option, index) => (
+          <option key={index} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
     </div>
   );
 };
 
 Dropdown.propTypes = {
+  label: PropTypes.string.isRequired,
   options: PropTypes.arrayOf(PropTypes.string).isRequired,
-  onSelect: PropTypes.func.isRequired,
+  selectedValue: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
 };
