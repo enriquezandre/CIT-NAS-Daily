b1dcb1f139eb835c988350dc7b5cd8fb9178779c
Andre Enriquez <andre.enriquez@cit.edu>
2023-12-07 00:11:24 +0800

    Update activities summary endpoint on superior side

--- frontend/cit-nas-daily/src/components/Superior/PerfSummary.jsx
+++ frontend/cit-nas-daily/src/components/Superior/PerfSummary.jsx
@@ -60,7 +60,11 @@ export const PerfSummary = ({
   useEffect(() => {
     const fetchActivitiesSummary = async () => {
       try {
-        const response = await api.get(`/ActivitiesSummary/${nasId}`);
+        const response = await api.get(
+          `/ActivitiesSummary/${nasId}/${selectedSY}/${getSemesterValue(
+            selectedSem
+          )}`
+        );
         const data = response.data;
         setActivitySummaries(data);
         setFilteredSummaries(data);
@@ -71,7 +75,7 @@ export const PerfSummary = ({
     };
 
     fetchActivitiesSummary();
-  }, [nasId, api]);
+  }, [nasId, selectedSY, selectedSem, api]);
 
   useEffect(() => {
     const fetchTimekeepingSummary = async () => {
