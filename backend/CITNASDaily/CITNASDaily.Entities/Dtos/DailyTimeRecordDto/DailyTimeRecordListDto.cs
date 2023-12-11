a4c9db7655e3cb4bab67d7d83ada0d99c33e0856
Jane Carla Barcoma <janecarla.barcoma@cit.edu>
2023-12-11 17:23:37 +0800

    Update DTR Models

--- backend/CITNASDaily/CITNASDaily.Entities/Dtos/DailyTimeRecordDto/DailyTimeRecordListDto.cs
+++ backend/CITNASDaily/CITNASDaily.Entities/Dtos/DailyTimeRecordDto/DailyTimeRecordListDto.cs
@@ -1,19 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CITNASDaily.Entities.Dtos.DailyTimeRecordDto;
 using static CITNASDaily.Entities.Enums.Enums;
 
-namespace CITNASDaily.Entities.Dtos.DailyTimeRecordDto
+public class DailyTimeRecordListDto
 {
-    public class DailyTimeRecordListDto
-    {
-        public string? FirstName { get; set; }
-        public string? MiddleName { get; set; }
-        public string? LastName { get; set; }
-        public Semester Semester { get; set; }
-        public int SchoolYear { get; set; }
-        public List<DailyTimeRecordDto>? DailyTimeRecords { get; set;}
-    }
+    public string? LastName { get; set; }
+    public string? MiddleName { get; set; }
+    public string? FirstName { get; set; }
+    public Semester Semester { get; set; }
+    public int SchoolYear { get; set; }
+    public List<DailyTimeRecordDto>? DailyTimeRecords { get; set; }
 }
