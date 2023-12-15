00fa7f31a3246bf3f71c1a562c23ed6297599adf
Andre Enriquez <andre.enriquez@cit.edu>
2023-12-15 23:06:48 +0800

    Update Add Superior form

--- frontend/cit-nas-daily/src/components/OAS/AddSuperiorForm.jsx
+++ frontend/cit-nas-daily/src/components/OAS/AddSuperiorForm.jsx
@@ -1,32 +1,71 @@
-// import { useMemo } from "react";
-// import axios from "axios";
+import { useMemo, useRef, useEffect, useState } from "react";
+import axios from "axios";
 
 export const AddSuperiorForm = () => {
-  //   const api = useMemo(
-  //     () =>
-  //       axios.create({
-  //         baseURL: "https://localhost:7001/api",
-  //         headers: {
-  //           Authorization: `Bearer ${localStorage.getItem("token")}`,
-  //         },
-  //       }),
-  //     []
-  //   );
+  const [offices, setOffices] = useState([]);
+  const lastnameRef = useRef();
+  const firstnameRef = useRef();
+  const officeRef = useRef();
 
-  //   const handleSubmit = async (event) => {
-  //     event.preventDefault();
+  const api = useMemo(
+    () =>
+      axios.create({
+        baseURL: "https://localhost:7001/api",
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      }),
+    []
+  );
 
-  //     try {
-  //       const registeruser = await api.post(`/Auth/register/`, {
-  //         username: 0,
-  //         password: 0,
-  //         role: totalFailedToPunch,
-  //       });
-  //       console.log(registeruser);
-  //     } catch (error) {
-  //       console.error(error);
-  //     }
-  //   };
+  useEffect(() => {
+    const fetchOffices = async () => {
+      try {
+        const response = await api.get(`/Offices`);
+        setOffices(response.data);
+        console.log(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchOffices();
+  }, [api]);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const lastname = lastnameRef.current.value;
+    const firstname = firstnameRef.current.value;
+    const officeId = officeRef.current.value;
+
+    const username = `${firstname}${lastname}`.toLowerCase();
+    console.log(username);
+    console.log(officeId);
+
+    // REGISTER AS USER
+    //   try {
+    //     const registeruser = await api.post(`/Auth/register/`, {
+    //       username: username,
+    //       password: username,
+    //       role: "Superior",
+    //     });
+    //     console.log(registeruser);
+    //   } catch (error) {
+    //     console.error(error);
+    //   }
+
+    //   REGISTER AS SUPERIOR
+    // try {
+    //   const registersuperior = await api.post(`/Superiors`, {
+    //     firstName: firstname,
+    //     lastName: lastname,
+    //     username: username,
+    //   });
+    //   console.log(registersuperior);
+    // } catch (error) {
+    //   console.error(error);
+    // }
+  };
 
   return (
     <div>
@@ -40,6 +79,7 @@ export const AddSuperiorForm = () => {
                   Last Name
                 </label>
                 <input
+                  ref={lastnameRef}
                   type="text"
                   name="lastname"
                   id="lastname"
@@ -53,6 +93,7 @@ export const AddSuperiorForm = () => {
                   First Name
                 </label>
                 <input
+                  ref={firstnameRef}
                   type="text"
                   name="firstname"
                   id="firstname"
@@ -66,20 +107,23 @@ export const AddSuperiorForm = () => {
                   Assigned Office
                 </label>
                 <select
+                  ref={officeRef}
                   id="category"
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5"
                 >
                   <option selected="">Select office</option>
-                  <option value="TV">TV/Monitors</option>
-                  <option value="PC">PC</option>
-                  <option value="GA">Gaming/Console</option>
-                  <option value="PH">Phones</option>
+                  {offices.map((office) => (
+                    <option key={office.id} value={office.id}>
+                      {office.name}
+                    </option>
+                  ))}
                 </select>
               </div>
             </div>
             <button
               type="submit"
               className="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-black bg-secondary rounded-lg focus:ring-4 focus:ring-primary-200 hover:bg-primary hover:text-white"
+              onClick={handleSubmit}
             >
               Add
             </button>
