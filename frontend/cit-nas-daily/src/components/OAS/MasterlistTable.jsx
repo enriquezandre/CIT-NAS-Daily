71d19d3a3650fd8afd0e74b420f3015cb4480c28
Andre Enriquez <andre.enriquez@cit.edu>
2024-01-08 03:12:52 +0800

    Add update icon on masterlist

--- frontend/cit-nas-daily/src/components/OAS/MasterlistTable.jsx
+++ frontend/cit-nas-daily/src/components/OAS/MasterlistTable.jsx
@@ -37,6 +37,7 @@ export const MasterlistTable = ({ searchInput, selectedSY, selectedSem, submitte
     "FOR MAKE UP",
     "OT",
     "REMARKS",
+    "Update",
   ];
 
   function getSemesterValue(sem) {
@@ -194,6 +195,24 @@ export const MasterlistTable = ({ searchInput, selectedSY, selectedSem, submitte
               </td>
               <td className="border-2 border-black text-center px-1 py-2 text-xs"> </td>
               <td className="border-2 border-black text-center px-1 py-2 text-xs"> </td>
+              <td className="border-2 border-black text-xs">
+                <div className="flex justify-center items-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6 cursor-pointer hover:transition-colors hover:text-primary"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
+                    />
+                  </svg>
+                </div>
+              </td>
             </tr>
           ))}
         </tbody>
