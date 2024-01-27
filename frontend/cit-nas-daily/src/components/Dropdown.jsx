f01a56022f48a9e7f5054416162353f851789cf2
Andre Enriquez <andre.enriquez@cit.edu>
2024-01-28 01:10:03 +0800

    Add mobresp oas attendance page

--- frontend/cit-nas-daily/src/components/Dropdown.jsx
+++ frontend/cit-nas-daily/src/components/Dropdown.jsx
@@ -2,12 +2,12 @@ import PropTypes from "prop-types";
 
 export const Dropdown = ({ label, options, selectedValue, onChange }) => {
   return (
-    <div className="flex flex-col sm:flex-row gap-2 items-center">
-      <div className="mr-2">{label}:</div>
+    <div className="flex flex-col sm:flex-row gap-1 md:gap-2 items-center">
+      <div className="mr-1 lg:mr-2">{label}:</div>
       <select
         value={selectedValue}
         onChange={onChange}
-        className="w-full text-base border rounded-md sm:w-[7rem]"
+        className="w-auto text-sm md:text-base border rounded-md sm:w-[5.5rem]"
       >
         {options.map((option, index) => (
           <option key={index} value={option}>
--- frontend/cit-nas-daily/src/components/MonthlySummary.jsx
+++ frontend/cit-nas-daily/src/components/MonthlySummary.jsx
@@ -5,34 +5,46 @@ export const MonthlySummary = ({ timekeepingSummaries }) => {
     <table className="w-4/5 mx-auto mb-8">
       <tbody>
         <tr>
-          <td className="w-2/5 border px-4 py-2">Make-up Duty Hours</td>
-          <td className="border px-4 py-2 text-center">
+          <td className="w-2/5 border px-4 py-2 text-xs md:text-base lg:text-lg">
+            Make-up Duty Hours
+          </td>
+          <td className="border px-4 py-2 text-center text-xs md:text-base lg:text-lg">
             {timekeepingSummaries.makeUpDutyHours !== null
               ? timekeepingSummaries.makeUpDutyHours
               : 0}
           </td>
-          <td className="w-2/5 border px-4 py-2">Late {">"} 10 Minutes</td>
-          <td className="border px-4 py-2 text-center">
+          <td className="w-2/5 border px-4 py-2 text-xs md:text-base lg:text-lg">
+            Late {">"} 10 Minutes
+          </td>
+          <td className="border px-4 py-2 text-center text-xs md:text-base lg:text-lg">
             {timekeepingSummaries.lateOver10Mins !== null ? timekeepingSummaries.lateOver10Mins : 0}
           </td>
         </tr>
         <tr>
-          <td className="border px-4 py-2">Number of Unexcused Absences</td>
-          <td className="border px-4 py-2 text-center">
+          <td className="border px-4 py-2 text-xs md:text-base lg:text-lg">
+            Number of Unexcused Absences
+          </td>
+          <td className="border px-4 py-2 text-center text-xs md:text-base lg:text-lg">
             {timekeepingSummaries.unexcused !== null ? timekeepingSummaries.unexcused : 0}
           </td>
-          <td className="border px-4 py-2">Late {">"} 45 Minutes </td>
-          <td className="border px-4 py-2 text-center">
+          <td className="border px-4 py-2 text-xs md:text-base lg:text-lg">
+            Late {">"} 45 Minutes{" "}
+          </td>
+          <td className="border px-4 py-2 text-center text-xs md:text-base lg:text-lg">
             {timekeepingSummaries.lateOver45Mins !== null ? timekeepingSummaries.lateOver45Mins : 0}
           </td>
         </tr>
         <tr>
-          <td className="border px-4 py-2">Number of Excused Absences</td>
-          <td className="border px-4 py-2 text-center">
+          <td className="border px-4 py-2 text-xs md:text-base lg:text-lg">
+            Number of Excused Absences
+          </td>
+          <td className="border px-4 py-2 text-center text-xs md:text-base lg:text-lg">
             {timekeepingSummaries.excused !== null ? timekeepingSummaries.excused : 0}
           </td>
-          <td className="border px-4 py-2">FTP - Failure to Punch In/Out</td>
-          <td className="border px-4 py-2 text-center">
+          <td className="border px-4 py-2 text-xs md:text-base lg:text-lg">
+            FTP - Failure to Punch In/Out
+          </td>
+          <td className="border px-4 py-2 text-center text-xs md:text-base lg:text-lg">
             {timekeepingSummaries.failedToPunch !== null ? timekeepingSummaries.failedToPunch : 0}
           </td>
         </tr>
