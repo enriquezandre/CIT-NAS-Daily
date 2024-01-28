58a5f400dd01dfb866a08cc5b5a03a54aa15c5d3
Stella Mariz Palen <stellamariz.palen@cit.edu>
2024-01-29 07:01:56 +0800

    Make the Attendance Summary Page Responsive

--- frontend/cit-nas-daily/src/components/DataDisplayBox.jsx
+++ frontend/cit-nas-daily/src/components/DataDisplayBox.jsx
@@ -2,9 +2,9 @@ import PropTypes from "prop-types";
 
 export const DataDisplayBox = ({ label, data }) => {
   return (
-    <div className="w-4/5 bg-white shadow-md flex m-1 text-sm">
+    <div className="w-full md:w-4/5 bg-white shadow-md flex md:m-1 text-sm">
       <div className="w-4/5">
-        <div className="bg-primary p-2 text-white rounded-l-lg">{label}</div>
+        <div className="bg-primary p-2 h-full text-white rounded-l-lg flex items-center">{label}</div>
       </div>
       <div className="bg-secondary w-1/5 p-2 rounded-r-lg">
         <div className="flex items-center justify-center">{data}</div>
