bcf73245b3c071fda52ab7de3981b628e26e6b86
Kaye Belderol <kayecassandra.belderol@cit.edu>
2024-03-23 00:19:11 +0800

    Remove delete button on view schedule

--- frontend/cit-nas-daily/src/components/NAS/ViewScheduleTable.jsx
+++ frontend/cit-nas-daily/src/components/NAS/ViewScheduleTable.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import axios from "axios";
 
-export const ViewScheduleTable = ({ nasId, schoolYear, semester, openModal }) => {
+export const ViewScheduleTable = ({ nasId, schoolYear, semester }) => {
   const [totalHours, setTotalHours] = useState(0);
   const [schedule, setSchedule] = useState({
     Monday: { items: [], hasSchedule: false },
@@ -167,34 +167,11 @@ export const ViewScheduleTable = ({ nasId, schoolYear, semester, openModal }) =>
           </tbody>
         </table>
       </div>
-      <div style={{ display: "flex", justifyContent: "center" }}>
-        <button
-          className="btn btn-secondary bg-primary text-white min-[450px]:justify-start flex items-center px-3 py-1 md:px-4 md:py-2 rounded-lg md:m-1 text-sm md:text-base hover:bg-secondary hover:text-black font-normal mr-3"
-          onClick={openModal}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="w-5 h-5 mr-2"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
-            />
-          </svg>
-          <span>Delete schedule</span>
-        </button>
-      </div>
     </div>
   );
 };
 
 ViewScheduleTable.propTypes = {
-  openModal: PropTypes.func.isRequired,
   currentMonth: PropTypes.string.isRequired,
   schoolYear: PropTypes.string.isRequired,
   semester: PropTypes.string.isRequired,
